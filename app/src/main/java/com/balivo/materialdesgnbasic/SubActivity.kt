4744ca4041988ad6def4e216ec22979f2093544f
Mihail Mihov <mihail4o@gmail.com>
2018-03-16 22:17:21 -0700

    Add actions to the Toolbar

--- app/src/main/java/com/balivo/materialdesgnbasic/SubActivity.kt
+++ app/src/main/java/com/balivo/materialdesgnbasic/SubActivity.kt
@@ -1,30 +1,29 @@
 package com.balivo.materialdesgnbasic
 
-import android.R.menu
-import android.content.Intent
-import android.support.v7.app.*
+import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
+import android.support.v4.app.NavUtils
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
 
 
-class MainActivity : AppCompatActivity() {
-
-    private var toolbar:Toolbar?=null
+class SubActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sub)
 
-        toolbar=findViewById(R.id.app_bar)
+        var toolbar = findViewById(R.id.app_bar) as Toolbar
         setSupportActionBar(toolbar)
+
+        getSupportActionBar()?.setHomeButtonEnabled(true)
+        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
+        menuInflater.inflate(R.menu.menu_sub, menu)
         return true
     }
 
@@ -32,20 +31,17 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        val id = item.itemId
+        val id = item.getItemId()
 
 
         if (id == R.id.action_settings) {
-
-            Toast.makeText(this, "Hey, you just hit!", Toast.LENGTH_LONG).show()
             return true
         }
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this)
+        }
 
-        else if (id == R.id.navigate) {
-
-            startActivity(Intent(this, SubActivity::class.java))
-            return true
-
-        } else return super.onOptionsItemSelected(item)
+        return super.onOptionsItemSelected(item)
     }
+
 }
